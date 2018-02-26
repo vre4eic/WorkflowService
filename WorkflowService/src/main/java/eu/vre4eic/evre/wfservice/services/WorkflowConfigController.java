9a638e343146d2fd71046f78d86d320641f49831
Cesare <cesare.concordia@isti.cnr.it>
2018-02-26 17:13:58 +0100

    Copyright message fixed

--- WorkflowService/src/main/java/eu/vre4eic/evre/wfservice/services/WorkflowConfigController.java
+++ WorkflowService/src/main/java/eu/vre4eic/evre/wfservice/services/WorkflowConfigController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 VRE4EIC Consortium
+ * Copyright (c) 2018 VRE4EIC Consortium
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
